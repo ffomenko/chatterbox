3a19dcc48e30c51562cea6d3126fe2d225dfade6
Fedor Fomenko <ffomenko@yahoo.co.uk>
2012-01-03 17:23:01 +0000

    Images storage on S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ActiveRecord::Base
   validates :body, :presence => true
   validates :poster, :presence => true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image,
+                    :styles => { :medium => "300x300>" },
+                    :path => ":id/:style/:basename.:extension",
+                    :storage => :s3,
+                    :s3_credentials => { :access_key_id     => ENV['S3_KEY'],
+                                         :secret_access_key => ENV['S3_SECRET']},
+                    :bucket => "ffchatterbox"
   validates_attachment_content_type :image, :content_type=>['image/jpeg', 'image/png']
 end
